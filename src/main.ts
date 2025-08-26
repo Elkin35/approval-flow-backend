553611047069f3e20a2a39db75ecacc7ff969a6a
Elkin Cuello <e.cuello@uniandes.edu.co>
2025-08-26 01:24:37 -0500

    add missing crypto import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeedingService } from './database/seeding/seeding.service';
+import 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
